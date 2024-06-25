bea78cff7df8f36eb2ca73c361e1ac260398bcf4
Ola Ganiyu <ganiyumuftau@mail.com>
2024-06-25 22:59:01 +0000

    word.txt added

--- run.py
+++ run.py
@@ -1,36 +1,29 @@
 import random
+import pandas as pd
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('Hangman')
+file = open('word.txt', 'r', encoding='utf-8')
+print(file.read())
+def get_answer_from_file():
+    """
+    Open text file and get random word for the game answer.
+    """
+    file = open('word.txt', 'r', encoding='utf-8')
+    lines = file.read().splitlines()
+    random_word = random.choice(lines)
+    return random_word.lower()
 
 # Define categories and words for each category
-countries = ["nigeria", "england", "ireland", "spain", "italy", "france"]
 
-hangman = ['''
-    +---+
-        |
-        |
-        |
-    ===''', '''
-    +---+
-     O   |
-    /|\  |
-    / \  |
-    ===''']
-
-max_wrong = len(hangman) - 1
-
-#inittialize variable 
-#pick a word 
-country =random.choice(countries)  
-
-#dashes for each letter in a word 
-current_guess = "-" * len(country)
-
-#wrong guess counter 
-wrong_guesses = 0
-
-#used letters checked 
-used_letters = []
-
-#loop Game Play 
-print("Welcome to Hangman Game")
-print(hangman)
 
