ea232ea0fb4b3ffdb659590834771a73c09672bf
Ola Ganiyu <ganiyumuftau@mail.com>
2024-07-03 19:36:21 +0000

    add requirement.txt

--- run.py
+++ run.py
@@ -24,6 +24,28 @@ def print_logo():
 ----------------------------------------------"""
     print(logo)
 
+
+def print_instruction():
+    instruction = r"""
+    ------------------------------------------------
+    ------------------------------------------------
+    --                 How to Play                --
+    --                                            --
+    -- Guess the random contry in 6 or less tries --
+    --      Each guess must be a valid letter     --
+    --                                            --
+    --     if guess wrong, live color changes     --
+    --                                            --
+    --                                            --
+    --                                            --
+    --                                            --
+    --                 GOOD LUCK                  --
+    --                                            --
+    --                                            --
+    ------------------------------------------------
+    ------------------------------------------------
+    """ 
+    print(instruction)
 import random 
 # Open the file in read mode 
 
@@ -87,86 +109,110 @@ def hangman(wrong):
             / \ |
             ===""")
 
+yes_check = "yes"
+no_check ="no"
+# check name for validation 
+def validate_name(name):
+    
+    while True:
+        name = input("Enter your name: ")
+        if len(name) <= 1 or not name.isalpha():
+            print("Name must be more than 1 letter and cannot be a number")
+            continue
+        if name.strip():  # Check name if empty
+            fore = Fore.BLUE + Style.BRIGHT
+            fore_reset = Fore.RESET
+            print(f"\nHello {fore}{name}{fore_reset}, Time to play hangman! \n")
+            
+            print_instruction()
+            while True:
+                play = input("press yes to continue or no to exit : \n").lower()
+                if play in yes_check:
+                    break
+                if play in no_check:
+                    exit()
+                if play is not yes_check and play is not no_check :
+                    print(Fore.RED+"please enter the correct letter")
+                    print(" " + Fore.RESET)
+                    continue
+            break
+            return name
+        else:
+            print(Fore.RED+"Please enter a valid name.")
+            print(" " + Fore.RESET)   
 
-def game_play():
+def game_start():
     styled_text=pyfiglet.figlet_format('Welcome to Hangman !',font= 'doom')
     print(styled_text)
     #print("Welcome to Hangman!")
     print_logo()
-    
-    # check name for validation 
-    while True:
-        name = input("Enter your name: ")
-        if name.strip():  # Check name if empty
-            print(f"\nHello {name}, Time to play hangman! \n")
-            break
-        else:
-            print("Please enter a valid name.")
-
+    name1 = ""
+    name = validate_name(name1)
     word1 =""
     answer = word_choice(word1)
-    if not answer:
-        return
-    print("------------------------------------------")
-    print("--------------Game Start-------------------")
-    guessLetter = []
-    incorrectLetter= []
-    live = 6
-    guess_word =['_']*len(answer)
-
-     # Main game loop
-    while live > 0 and '_' in guess_word:
-        # Display current progress
-        print(" ".join(guess_word))
-        print("Let Play: ", ", ".join(incorrectLetter))
-        hangman(6 - live)  # Display the hangman
-        print(f"Tries left: {live}")
-        guess = input("Guess a letter: \n").lower()
-        # Validate the guess
-        if len(guess) != 1 or not guess.isalpha():
-            print("Please enter a single letter.")
-            continue
-
-        if guess in guessLetter:
-            print("You've already guessed that letter.")
-            continue
-            # Check if the guessed letter is in the word
-        guessLetter.append(guess)
-
-        if guess in answer:
-            for i in range(len(answer)):
-                if answer[i] == guess:
-                    guess_word[i] = guess
-            print("Correct!")
-        else:
-                live -= 1
-                incorrectLetter.append(guess)
-                print("Incorrect!")
-
-        # Game result
-    if '_' not in guess_word:
-        print(f"Congratulations, {name}! You've guessed the word:", answer)
-        ("-------------------WELL DONE-------------------------")
-    else:
-        print("Game over, you lose! The word was:", answer)
-        hangman(6)  # Display the last hanged man when losing
-
-    # Ask if the player wants to play again
-    play_again = input("Do you want to play again? (yes/no):\n ").lower()
-
-    if play_again != "yes":
-        while true:
-            if play_again == "no":
-                print(pyfiglet.figlet_format(
-                        "GOODBYE", justify="center", width=80))
-                print(f"Thank you for playing, {name}! Have a nice day!")
-                return
-            else :
-                print("Please enter either (yes/no).")
-    if play_again == "yes":
+    
+    def game_play():
+        if not answer:
+            return
         print("--------------Game Start-------------------")
-        game_play()
+        guessLetter = []
+        incorrectLetter= []
+        live = 6
+        guess_word =['_']*len(answer)
 
+        # Main game loop
+        while live > 0 and '_' in guess_word:
+            # Display current progress
+            print(" ".join(guess_word))
+            print("Let Play: ", ", ".join(incorrectLetter))
+            hangman(6 - live)  # Display the hangman
+            fore = Fore.YELLOW + Style.BRIGHT
+            print(f"Tries left: {fore}{live}")
+            print(" " + Fore.RESET)
+            guess = input("Guess a letter: \n").lower()
+            # Validate the guess
+            if len(guess) != 1 or not guess.isalpha():
+                print("Please enter a single letter.")
+                continue
 
+            if guess in guessLetter:
+                print("You've already guessed that letter.")
+                continue
+                # Check if the guessed letter is in the word
+            guessLetter.append(guess)
+
+            if guess in answer:
+                for i in range(len(answer)):
+                    if answer[i] == guess:
+                        guess_word[i] = guess
+                print("Correct!")
+            else:
+                    live -= 1
+                    incorrectLetter.append(guess)
+                    print(Fore.RED+"Incorrect!")
+                    print(" " + Fore.RESET)
+
+            # Game result
+        if '_' not in guess_word:
+            print(f"Congratulations, {name}! You've guessed the word:", answer)
+            ("-------------------WELL DONE-------------------------")
+        else:
+            print("Game over, you lose! The word was:", answer)
+            hangman(6)  # Display the last hanged man when losing
+        while True:
+            # Ask if the player wants to play again
+            play_again = input("Do you want to play again? (yes/no):\n ").lower()
+            if play_again in yes_check:
+                game_play()
+            if play_again in no_check:
+                print(pyfiglet.figlet_format(
+                "GOODBYE", justify="center", width=80))
+                print(f"Thank you for playing, {name}! Have a nice day!")
+                exit()
+            if play_again is not yes_check and play_again is not no_check :
+                print(Fore.RED+"please enter the correct letter")
+                print(" " + Fore.RESET)
+                continue
+    game_play()
 # Play the game
-game_play()
+game_start()
